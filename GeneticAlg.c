3002a58dc832886d82a930841033b233b236421c
justinbacic <justinbacic@gmail.com>
2025-03-11 21:00:31 -0400

    Completed the inital generation creation, formatted the file so it was more readable

--- GeneticAlg.c
+++ GeneticAlg.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
-#define populationSize 3
+#define populationSize 10
 #define mutationFactor 0.2
 #define mutationProbability 0.05
 //These define the ranges that we want the randomly generated first generation to be in
@@ -13,38 +13,29 @@
 #define kiMax 10
 #define kdMin 1
 #define kdMax 10
+
+//***STRUCTS***
+
 typedef struct{
     double kp;
     double ki;
     double kd;
     double fitnessValue;
 }valueTuple;
+
 typedef struct{
     valueTuple generationValues[populationSize];
 }generation;
+
+//***UTILITY FUNCTIONS***
 void printTuple(valueTuple tup){
     printf("Kp=%lf, Ki=%lf, Kd=%lf, Fitness Value=%lf\n", tup.kp, tup.ki, tup.kd, tup.fitnessValue);
-}
-//Run fitness function on a specific tuple
-valueTuple fitnessFunction(valueTuple tup){
-    double fitness = (tup.kp * 2) - (tup.ki * 1.5) / (tup.kd * 0.34);
-    tup.fitnessValue = fitness;
-    valueTuple newTup = {tup.kp, tup.ki, tup.kd, fitness};
-    return newTup;
-}
-//Run fitness function on a whole generation
-generation fitnessFunctionGeneration(generation gen){
-    for(int i=0;i<populationSize;i++){
-        gen.generationValues[i] = fitnessFunction(gen.generationValues[i]);
-    }
-    return gen;
 }
 void printGeneration(generation gen){
     for(int i=0;i<populationSize;i++){
         printTuple(gen.generationValues[i]);
     }
 }
-// Function to generate a random permutation in the range [min, max]
 void generatePermutation(int min, int max, int result[]) {
     int size = max - min + 1;
     for (int i = 0; i < size; i++) {
@@ -59,8 +50,28 @@ void generatePermutation(int min, int max, int result[]) {
         result[j] = temp;
     }
 }
+
+//***GENETIC ALGORITHM FUNCTIONS***
+
+//Run fitness function on a specific tuple, we will change this once we can run our hardware
+valueTuple fitnessFunction(valueTuple tup){
+    double fitness = (tup.kp * 2) - (tup.ki * 1.5) / (tup.kd * 0.34); //Function I chose completely on random
+    tup.fitnessValue = fitness;
+    valueTuple newTup = {tup.kp, tup.ki, tup.kd, fitness};
+    return newTup;
+}
+//Runs fitness function on a whole generation
+generation fitnessFunctionGeneration(generation gen){
+    for(int i=0;i<populationSize;i++){
+        gen.generationValues[i] = fitnessFunction(gen.generationValues[i]);
+    }
+    return gen;
+}
+
+//Preform the selection, crossover and mutation steps of the genetic algorithm
 generation nextGeneration(generation currentGeneration){
     generation newGeneration;
+    //SELECTION STARTS HERE
     double bestFitnessValue = INFINITY;
     int bestFitnessIndex = 0;
     int index = 0;
@@ -86,16 +97,14 @@ generation nextGeneration(generation currentGeneration){
     //printTuple(currentGeneration.generationValues[bestFitnessIndex]);
     newGeneration.generationValues[0].fitnessValue = -1;
     
+    //CROSSOVER STARTS HERE
     //Shuffles by creating a random permutation of 1 to populationSize - 1 for each of the attributes
-    int kpIndices[populationSize];  // Array to store the permutation for kp
-    generatePermutation(0, populationSize-1, kpIndices);
+    int kpIndices[populationSize-1];  // Array to store the permutation for kp
+    generatePermutation(1, populationSize-1, kpIndices);
     int kiIndices[populationSize];  // Array to store the permutation for ki
-    generatePermutation(0, populationSize-1, kiIndices);
+    generatePermutation(1, populationSize-1, kiIndices);
     int kdIndices[populationSize];  // Array to store the permutation for kd
-    generatePermutation(0, populationSize-1, kdIndices);
-
-    //Note on the shuffling: Originally I had the shuffling so that it only swapped between the bottom n-1 tuples
-    //but including the possibility for the tuples to take copy values from the best preforming increased the development a lot
+    generatePermutation(1, populationSize-1, kdIndices);
 
     for(int i=1; i<populationSize;i++){
         //Shuffles the kp values
@@ -106,6 +115,7 @@ generation nextGeneration(generation currentGeneration){
         newGeneration.generationValues[i].kd = currentGeneration.generationValues[kdIndices[i-1]].kd;
     }
 
+    //MUTATION STARTS HERE
     //Mutation of the values, this is controlled by the mutation factor which says how much the value will change
     //The mutation happens with a set probability for each individual value
     for(int i=1;i<populationSize;i++){
@@ -120,19 +130,17 @@ generation nextGeneration(generation currentGeneration){
             }else{ //Decrease
                 newGeneration.generationValues[i].kp = (1+0.2)*newGeneration.generationValues[i].kp;
             }
-            printf("Mutating with val %d\n",randNum);
         }
         //Determine if ki should mutate
         randNum = rand() % (100 + 1);
         if(randNum <= (mutationProbability*100)){
+            //Determine if increase or decrease
             randNum = rand() % (2);
             if(randNum){ //Increase
                 newGeneration.generationValues[i].ki = (1-0.2)*newGeneration.generationValues[i].ki; 
             }else{ //Decrease
                 newGeneration.generationValues[i].ki = (1+0.2)*newGeneration.generationValues[i].ki;
             }
-            //Determine if increase or decrease
-            printf("Mutating with val %d\n",randNum);
         }
         //Determine if kd should mutate
         randNum = rand() % (100 + 1);
@@ -144,26 +152,47 @@ generation nextGeneration(generation currentGeneration){
             }else{ //Decrease
                 newGeneration.generationValues[i].kd = (1+0.2)*newGeneration.generationValues[i].kd;
             }
-            printf("Mutating with val %d\n",randNum);
         }
     }
     printf("Current lowest value:%lf\n", bestFitnessValue);
     return newGeneration; 
 };
 
+//Creates our first generation for values
+generation initalGeneration(){
+    generation initalGen;
+    //Initialize the values of the array
+    for(int i=0;i<populationSize;i++){
+        valueTuple currentVal;
+        currentVal.kp = -1;
+        currentVal.ki = -1;
+        currentVal.kd = -1;
+        currentVal.fitnessValue = -1;
+        initalGen.generationValues[i] = currentVal;
+    }
+    //Create the values randomly within the ranges set
+    //NEED TO CHANGE: when we set actual max and min we need to convert the rand ints to sufficiently sized doubles
+    for(int i=0; i<populationSize;i++){
+        double kp = rand() % (kpMax) + kpMin;
+        initalGen.generationValues[i].kp = kp;
+        double ki = rand() % (kiMax) + kiMin;
+        initalGen.generationValues[i].ki = ki;
+        double kd = rand() % (kdMax) + kdMin;
+        initalGen.generationValues[i].kd = kd;
+    }
+    return initalGen;
+
+}
+
 int main(){
-    srand(time(NULL));
-    valueTuple a={2,3,4,-1};
-    valueTuple b={5,8,6,-1};
-    valueTuple c={1,1,9,-1};
-    generation gen = {{a,b,c}};
-    
+    srand(time(NULL)); //For seeding random num gen
+    generation gen = initalGeneration();
+    printGeneration(gen);
     for(int i=0;i<10;i++){
         gen = fitnessFunctionGeneration(gen);
         gen = nextGeneration(gen);
     }
     gen = fitnessFunctionGeneration(gen);
     printGeneration(gen);
-    //printf("Random Value: %d\n", randomValue);
     return 0;
 }
